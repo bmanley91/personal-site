ef5ababd84d0d17d03a141b1b5a9ff14de57d0c9
Brian Manley <brian.manley91@gmail.com>
2019-12-02 10:33:53 -0500

    Fixed typo.

--- src/resources/content/cool-stuff.ts
+++ src/resources/content/cool-stuff.ts
@@ -3,7 +3,7 @@ import { ContentCardProps } from '../../components/content-card';
 export const coolStuff: ContentCardProps[] = [
     {
         title: 'Undefined Podcast',
-        body: 'A podcast where two devs, Ken Wheeler and Jared Palmer, talk with other renounded engineers about cool stuff they\'ve worked on.',
+        body: 'A podcast where two devs, Ken Wheeler and Jared Palmer, talk with other renouned engineers about cool stuff they\'ve worked on.',
         linkText: 'Undefined Podcast on Spotify',
         linkURL: 'https://open.spotify.com/show/6Zwfp98CtaR6Wt1qklemEM'
     }
