e651e93f1d8e429d2868af08bc49a1ac87c730a6
bmanley91 <brian.manley91@gmail.com>
2020-05-29 17:03:13 -0400

    Fixed typo in crises

--- src/resources/content/learning-stuff.ts
+++ src/resources/content/learning-stuff.ts
@@ -3,7 +3,7 @@ import { ContentCardProps } from '../../components/content-card';
 export const learningStuff: ContentCardProps[] = [
     {
         title: 'Resilient Management by Lara Hogan',
-        body: 'As someone passionate about engineering leadership, I loved Lara Hogan\'s book Resilient Management. It goes over everything from how to craft and deliver feedback to dealing with your teammates\' personal crisises. I recommend this book for anyone interested in engineering management. I wrote up and saved some (full of typos) notes on this book in my github if you\'d like to see a bit of what this book covers.',
+        body: 'As someone passionate about engineering leadership, I loved Lara Hogan\'s book Resilient Management. It goes over everything from how to craft and deliver feedback to dealing with your teammates\' personal crises. I recommend this book for anyone interested in engineering management. I wrote up and saved some (full of typos) notes on this book in my github if you\'d like to see a bit of what this book covers.',
         linkText: "Check out the book here",
         linkURL: 'https://resilient-management.com/'
     }, {
