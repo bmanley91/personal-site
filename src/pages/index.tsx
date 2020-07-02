57bd522b364f606721abf1ac4e8d50f42eb49e90
bmanley91 <brian.manley91@gmail.com>
2020-07-01 21:52:01 -0400

    Added slick carousel.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,24 @@ import Layout from '../components/layout';
 import ContentList from '../components/content-list';
 import { myStuff, coolStuff, learningStuff } from '../resources/content';
 import { ThemeProvider } from '../components/theme-provider';
+import ContentCarousel from '../components/content-carousel';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 const Home: NextPage = () => {
     return (
         <ThemeProvider>
             <Layout title='Brian Manley - Home'>
                 <IntroSection />
-                <ContentList title="Stuff I've Worked On" contentCardPropArray={myStuff}/>
-                <ContentList title="Stuff I've Learned From" contentCardPropArray={learningStuff}/>
-                <ContentList title="Stuff That I Think is Cool " contentCardPropArray={coolStuff}/>
+                <MobileView>
+                    <ContentCarousel title="Stuff I've Worked On" contentCardPropArray={myStuff}/>
+                    <ContentCarousel title="Stuff I've Learned From" contentCardPropArray={learningStuff}/>
+                    <ContentCarousel title="Stuff That I Think is Cool " contentCardPropArray={coolStuff}/>
+                </MobileView>
+                <BrowserView>
+                    <ContentList title="Stuff I've Worked On" contentCardPropArray={myStuff}/>
+                    <ContentList title="Stuff I've Learned From" contentCardPropArray={learningStuff}/>
+                    <ContentList title="Stuff That I Think is Cool " contentCardPropArray={coolStuff}/>
+                </BrowserView>
             </Layout>
         </ThemeProvider>
     );
