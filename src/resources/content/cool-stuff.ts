a6e21fd075c0667c1e190bba7c8fae26c9939e80
bmanley91 <brian.manley91@gmail.com>
2020-07-01 22:00:50 -0400

    Added react-slick to cool stuff.

--- src/resources/content/cool-stuff.ts
+++ src/resources/content/cool-stuff.ts
@@ -6,5 +6,10 @@ export const coolStuff: ContentCardProps[] = [
         body: 'A podcast where two devs, Ken Wheeler and Jared Palmer, talk with other renowned engineers about cool stuff they\'ve worked on.',
         linkText: 'Undefined Podcast on Spotify',
         linkURL: 'https://open.spotify.com/show/6Zwfp98CtaR6Wt1qklemEM'
+    }, {
+        title: 'Slick Carousel',
+        body: 'The carousel component used to display these cards on mobile browsers.',
+        linkText: 'npm',
+        linkURL: 'https://www.npmjs.com/package/react-slick'
     }
 ];
