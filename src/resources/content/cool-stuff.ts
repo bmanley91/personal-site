c602db5d7947951bce958fa61e8d15ab3eadaa43
Brian Manley <brian.manley91@gmail.com>
2022-05-09 19:39:01 -0400

    Added new content

--- src/resources/content/cool-stuff.ts
+++ src/resources/content/cool-stuff.ts
@@ -2,6 +2,11 @@ import { ContentCardProps } from '../../components/content-card';
 
 export const coolStuff: ContentCardProps[] = [
     {
+        title: 'Progress - Ten Reasons to Look Forward to the Future',
+        body: 'A refreshingly positve, data-backed account of how things are actually getting better.',
+        linkText: 'Check out the book here',
+        linkURL: 'https://www.goodreads.com/book/show/31694126-progress---ten-reasons-to-look-forward-to-the-future?from_search=true&from_srp=true&qid=EoKlloWmjL&rank=20'
+    }, {
         title: 'Undefined Podcast',
         body: 'A podcast where two devs, Ken Wheeler and Jared Palmer, talk with other renowned engineers about cool stuff they\'ve worked on.',
         linkText: 'Undefined Podcast on Spotify',
--- src/resources/content/my-stuff.ts
+++ src/resources/content/my-stuff.ts
@@ -2,6 +2,11 @@ import { ContentCardProps } from '../../components/content-card';
 
 export const myStuff: ContentCardProps[] = [
     {
+        title: 'Effective Engineering Interviews',
+        body: 'To hire engineers you need a way to make sure they’re a fit for your team. Doing so efficiently and effectively is hard. This publication aims to dive into common engineering interview practices, see what makes them work, and how they can be improved.',
+        linkText: 'Check it out on Medium here',
+        linkURL: 'https://medium.com/effective-engineering-interviews'
+    }, {
         title: 'Headwind Golf',
         body: 'An iOS app that lets golfers know how the wind will affect their game. The app is built using SwiftUI and pulls data from the OpenWeather API.',
         linkText: 'Check it out on the App Store!',
@@ -12,16 +17,11 @@ export const myStuff: ContentCardProps[] = [
         linkText: 'Check out Cheatsheet on Github',
         linkURL: 'https://github.com/bmanley91/cheatsheet'
     }, {
-        title: 'Express i18n Made Easy',
-        body: 'A dev.to article I wrote outlining how to internationalize an Express API.',
-        linkText: 'Express i18n Made Easy',
-        linkURL: 'https://dev.to/bmanley91/express-i18n-made-easy-2d2o'
+        title: 'Poke Info',
+        body: 'An Alexa skill that tells you about pokemon!',
+        linkText: 'Check out the Alexa skill here',
+        linkURL: 'https://www.amazon.com/dp/B09SQ2MQV3'
     }, {
-        title: 'Gradle Cucumber JVM Plugin',
-        body: 'A Gradle plugin that allows for the execution of Cucumber tests. I added an option that the user can specify to customize their report format. This plugin was maintained by the CommerceHub OSS team, but has since stopped being maintained.',
-        linkText: 'gradle-cucumer-jvm-plugin',
-        linkURL: 'https://github.com/commercehub-oss/gradle-cucumber-jvm-plugin'
-    },{
         title: 'This Site',
         body: 'The website that you\'re looking at right now! It uses NextJs, React, and Emotion. It\'s hosted and deployed with Netlify.',
         linkText: 'Source Code',
@@ -36,3 +36,9 @@ export const myStuff: ContentCardProps[] = [
 //     linkText: 'gatling-gradle-plugin',
 //     linkURL: 'https://github.com/commercehub-oss/gatling-gradle-plugin'
 // }, 
+// {
+//     title: 'Gradle Cucumber JVM Plugin',
+//     body: 'A Gradle plugin that allows for the execution of Cucumber tests. I added an option that the user can specify to customize their report format. This plugin was maintained by the CommerceHub OSS team, but has since stopped being maintained.',
+//     linkText: 'gradle-cucumer-jvm-plugin',
+//     linkURL: 'https://github.com/commercehub-oss/gradle-cucumber-jvm-plugin'
+// },
